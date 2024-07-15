f8f563c24089f4cca600e0ae3f9ff1ac23aebce4
Rafael Cardoso <faelcardoso8@gmail.com>
2024-07-15 20:15:00 -0300

    15-07-24

--- algoritimos_mentoria_JS/converssor_de__algarismos_romanos/converssor_de _romanos.js
+++ algoritimos_mentoria_JS/converssor_de__algarismos_romanos/converssor_de _romanos.js
@@ -1,27 +1,42 @@
-let entrada = "X" 
-let valor_decimal = 0 
-let valor_num 
-let proximo_valor
+function romanos_para_cedimal(entrada){
 
-const romanos ={ // objeto com os valores romanos 
- I : 1,
- V : 5,
- X : 10,
- E : 50,
- C : 100,
- D : 500,
- M : 1000,
-}
 
-if( entrada.length >= 1 && entrada.length < 15){
+ let valor_decimal  = 0
+ let valor_num 
+ let proximo_valor
+
+ const romanos ={ // objeto com os valores romanos 
+  I : 1,
+  V : 5,
+  X : 10,
+  L : 50,
+  C : 100,
+  D : 500,
+  M : 1000,
+ }
+
+  if( entrada.length >= 1 && entrada.length < 15){
    for ( i = 0; i < entrada.length; i++){
     valor_num = romanos[entrada[i]]
     //console.log(valor_num)
-    
-   }  
-    
+    proximo_valor = romanos[entrada[i + 1]]
+
+    if(proximo_valor > valor_num){
+        valor_decimal -= valor_num
+    }
+    else{
+        valor_decimal += valor_num
+    }    
+   }    
+  return valor_decimal
+  }
+  else{
+    console.log("digite um valor válido")
+  }
 
 }
-else{
-    console.log("digite um valor válido")
-}
+
+console.log(romanos_para_cedimal("XVII"))
+console.log(romanos_para_cedimal("III"))
+console.log(romanos_para_cedimal("LVIII"))
+console.log(romanos_para_cedimal("MCMXCIV"))
---
+++ algoritimos_mentoria_JS/validação_de_palavras/validacao_de_palavras.js
@@ -0,0 +1,44 @@
+function verificar_palavra(entrada){
+     let verificar_esp 
+     let tamanho_palavra
+    
+    // testar o tamanho da palavra
+    if( entrada.length < 21){
+
+      if(entrada.length > 2){
+       tamanho_palavra = true
+
+      }
+      else{
+        tamanho_palavra = false
+      }
+    }
+    else{
+        tamanho_palavra = false
+    }
+
+    // testar caracteres especiais
+    for( i = 0; i < entrada.length; i++){
+        if (entrada[i].match(/[a-zA-Z0-9\s]/) && entrada[i] !== " "){// testa se tem vogais, consoantes e números usando expressões regulares
+            verificar_esp = true
+        }
+        else{
+            verificar_esp = false
+            break
+        }
+    }
+
+    //teste final- verificar o tamnho da palavra e se tem caractere especial
+    if( verificar_esp == true && tamanho_palavra == true){
+        return "verdadeiro"
+    }
+    else{
+        return "falso"
+    }    
+}
+
+
+console.log(verificar_palavra("234Adas"))
+console.log(verificar_palavra("b3"))
+console.log(verificar_palavra("fdbfbdbfdzfbfbfbfdbgdgbberrwewFYLÇFGHDTDSARW"))
+console.log(verificar_palavra("a3$e"))
